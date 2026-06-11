5da8d83a59083bdc69add9a796dfa3050a71891e
hildanku <hildankutomo@gmail.com>
2026-06-11 10:44:57 +0700

    docs(schemas): add docs about schemas

--- src/http/schemas.ts
+++ src/http/schemas.ts
@@ -1,5 +1,9 @@
 import { Schema } from 'effect'
-
+/* 
+This is the schema for the query parameters of the /recommend endpoint.
+It expects a 'judul' parameter which is a string with a minimum length of 1 and a maximum length of 100.
+It also has an optional 'topN' parameter which is a number that can be provided as a string, with a default value of 5 if not provided.
+*/
 export const RecommendQuerySchema = Schema.Struct({
 	judul: Schema.String.pipe(Schema.minLength(1), Schema.maxLength(100)),
 	topN: Schema.optionalWith(Schema.NumberFromString, { default: () => 5 }),
